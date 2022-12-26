e34638fd93a1a75904b416680bebe8f4a4f29f28
Woongineering <82315060+ChoiSeoWoong@users.noreply.github.com>
2022-12-27 00:31:07 +0900

    Create 프로_118667_두큐합.java
    221227 Commit

---
+++ James/221227_study/프로_118667_두큐합.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -2;
+        Queue<Integer> firstQueue = new LinkedList<>();
+        Queue<Integer> secondQueue = new LinkedList<>();
+        long firstQueueSum = 0, secondQueueSum = 0;
+        long halfTotal;
+        int maxCount = queue1.length * 3; // 교체할 수 있는 최대 횟수
+        int count = 0;
+        
+        for(int i = 0; i < queue1.length; i++) {
+            firstQueue.add(queue1[i]);
+            secondQueue.add(queue2[i]);
+            firstQueueSum += queue1[i];
+            secondQueueSum += queue2[i];
+        }
+        
+        halfTotal = (firstQueueSum + secondQueueSum) / 2;
+        
+        while(firstQueueSum != halfTotal) {
+             if(maxCount < 0) {
+                break;
+            }
+            
+            if(firstQueueSum > halfTotal) {
+                int firstQueueData = firstQueue.poll();
+                secondQueue.add(firstQueueData);
+                firstQueueSum -= firstQueueData;
+            }
+            
+            else if(firstQueueSum < halfTotal) {
+                int secondQueueData = secondQueue.poll();
+                firstQueue.add(secondQueueData);
+                firstQueueSum += secondQueueData;
+            }
+            
+            count++;
+            maxCount--;
+        }
+        
+        if(maxCount < 0) {
+            return -1;
+        }
+        
+        else {
+            answer = count;
+            return answer;
+        }
+    }
+}
