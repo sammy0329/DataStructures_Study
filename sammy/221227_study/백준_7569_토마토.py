0bd001b9ab88e3900d51c16f4e2fd05aa041015e
sammy <sammy0329@naver.com>
2022-12-31 23:19:51 +0900

    230103 study 문제 영어끝말잇기 풀이

--- sammy/221227_study/백준_7569_토마토.py
+++ sammy/221227_study/백준_7569_토마토.py
@@ -1,10 +1,11 @@
 # M은 상자의 가로 칸 수, N은 상자의 세로 칸 수, H는 상자 수
 import sys
 from collections import deque
+
 input=sys.stdin.readline
 M,N,H=map(int,input().split())
 
-graph=[[] for _ in range(H)]
+graph=[[] for _ in range(H)] 
 tomato=deque()
 cnt=0
 
@@ -17,18 +18,18 @@ for i in range(H):
     
     for j in range(N):
         data=list(map(int,input().split()))
-        cnt+=1
+        
         
         for idx,num in enumerate(data):
             if num==1:
-                tomato.append((j,idx,i))
+                tomato.append((j,idx,i)) # 1인 값 큐에 삽입
                 
-        graph[i].append(data)
+        graph[i].append(data) # 3차원 배열 만들기
         
-cnt=0
+
 
 while tomato:
-    x,y,z=tomato.popleft()
+    x,y,z=tomato.popleft() # 큐에서 꺼내기
     
     for i in range(6):
         nx=x+dx[i]
@@ -36,16 +37,14 @@ while tomato:
         nz=z+dz[i]
         
         
-        # 위치 벗어나면 안됨
+        # 위치 벗어나면 continue
         if nx<0 or nx>=N or ny<0 or ny>=M or nz<0 or nz>=H:
             continue
-        
-     
-        
-        if graph[nz][nx][ny]==-1:
+
+        if graph[nz][nx][ny]==-1: # 토마토가 없으면 continue
             continue
         
-        if graph[nz][nx][ny]==0:
+        if graph[nz][nx][ny]==0: # 0이면 1씩 증가시켜 나가기
             graph[nz][nx][ny]=graph[z][x][y]+1
             tomato.append((nx,ny,nz))
             
@@ -56,14 +55,12 @@ for i in range(H):
     for j in range(N):
          
         for k in range(M):
-            if graph[i][j][k]==0:
+            if graph[i][j][k]==0: # 0이 남아있는지 check
                 check=True
                 break
-        cnt=max(cnt,max(graph[i][j]))
+        cnt=max(cnt,max(graph[i][j])) # cnt에 최댓값 갱신
             
 if check:
     print(-1)
 else:
-    print(cnt)           
-
-print(graph)
+    print(cnt-1)           
