eb5e004e4a8cd391e85ac621305a6f4b40e11fe6
sammy <sammy0329@naver.com>
2023-01-09 23:22:38 +0900

    .

--- sammy/230105_study/프로_92341_주차 요금 계산.py
+++ sammy/230105_study/프로_92341_주차 요금 계산.py
@@ -1,5 +1,6 @@
 from collections import deque
 import math
+
 def solution(fees, records):
     answer = []
     inStatus={} # 입차 데이터 관리 딕셔너리
@@ -44,8 +45,7 @@ def solution(fees, records):
             answer.append(fees[1]+(int(math.ceil((check_time-fees[0])/fees[2])))*fees[3])
         else:
             answer.append(fees[1])
-        
-            
+                   
     return answer
 
 # print(solution([180, 5000, 10, 600],["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
