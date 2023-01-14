254a86735cd958e0bcdcf6540c98b613d4143f67
sammy <sammy0329@naver.com>
2023-01-14 10:05:51 +0900

    230112 study 양궁문제풀이

--- sammy/230112_study/백준_9663_NQueen.py
+++ sammy/230112_study/백준_9663_NQueen.py
@@ -1,47 +1,37 @@
+# 도전중
 import sys
 
-def backTracking(n,x,y):
+def backTracking(n,queencount,x,y):
+    global count
     
-    global count,queen_count
-    
-    if queen_count == n:
+    if queencount == n:
         count+=1
         return
-    if x<0 or x>n or y<0 or y>n: return
     
-    if visited[x][y]==True: return
+    if x<0 or x>n or y<0 or y>n:
+        return
+    
+    if visited[x][y]==True:
+        visited[x][y]=False
+        return
     
     if visited[x][y]==False:
         visited[x][y]=True
-        queen_count+=1
-            
-    for i in range(1,n):
-        x+=i
-        for j in range(1,n):  
-            y+=i
-            backTracking(n,x,y)
-            
-            y-=i
-        x-=i
-            
-        
-        
-    # if total>n: return # total 값이 n보다 클 경우 종료 조건
-    
-    # if total==n: # total 값이 n일 경우 count+1을 해주고 종료
-    #     count+=1
-    #     return
-    
-    # for i in range(1,4): # 1부터 3까지의 수들로 합을 만들기
-    #     total+=i
-    #     backTracking(n,total)
-    #     total-=i
-    
+        nx,ny=0,0
+        i=0
+        while True:
+            i+=1
+            nx=x+dx[i]
+            ny=y+dy[i]
+                  
+           
 input = sys.stdin.readline
 n=int(input())
 visited=[[False]*n for _ in range(n)]
 count=0
-queen_count=0
-backTracking(n,0,0)
-print(count)
 
+dx=[1,-1,0,0]
+dy=[0,0,1,-1]
+
+backTracking(n,0,0,0)
+print(count)
