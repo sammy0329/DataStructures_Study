ede13cc6bc52e683e3a2128e79e779cb52525ebc
sammy <sammy0329@naver.com>
2023-01-17 00:15:35 +0900

    230118 study 신고 결과 받기 문제풀이

--- sammy/230105_study/프로_42842_카펫.py
+++ sammy/230105_study/프로_42842_카펫.py
@@ -10,6 +10,6 @@ def solution(brown, yellow):
     
     return answer
 
-print(solution(10,2))
-print(solution(8,1))
-print(solution(24,24))
+# print(solution(10,2))
+# print(solution(8,1))
+# print(solution(24,24))
