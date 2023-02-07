fef7833bc4110f27049a48e6127c9a993db83804
Woongineering <82315060+ChoiSeoWoong@users.noreply.github.com>
2023-02-08 00:27:01 +0900

    230208 Commit

--- James/230202_study/프로_43164_여행경로.java
+++ James/230202_study/프로_43164_여행경로.java
@@ -1,1 +1,35 @@
+import java.util.*;
 
+class Solution {
+    static ArrayList<String> routeAl;
+    static boolean[] visited;
+    
+    public static void dfs(String start, String route, String[][] tickets, int cnt) {
+        if(cnt == tickets.length) {
+            routeAl.add(route);
+            return;
+        }
+        
+        for(int i = 0; i < tickets.length; i++) {
+            if(start.equals(tickets[i][0]) && !visited[i]) {
+                visited[i] = true;
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        visited = new boolean[tickets.length];
+        routeAl = new ArrayList<>();
+        
+        dfs("ICN", "ICN", tickets, 0);
+        
+        Collections.sort(routeAl);
+        // System.out.println(routeAl.toString());
+        
+        answer = routeAl.get(0).split(" ");
+        return answer;
+    }
+}
