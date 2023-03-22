5a6a7b05ba0910092d26bee7e61c4d7b1028238a
안성재 <anseongjae@anseongjaeui-MacBookAir.local>
2023-03-22 11:17:18 +0900

    230323 study [1차] 비밀지도 문제풀이

---
+++ sammy/230323_study/프로_17681_[1차] 비밀지도.py
@@ -0,0 +1,23 @@
+def solution(n, arr1, arr2):
+    answer = []
+
+    for i in range(n):
+        arr1_bin=bin(arr1[i])[2:]
+        arr2_bin=bin(arr2[i])[2:]
+
+        arr1_bin=arr1_bin.zfill(n)
+        arr2_bin=arr2_bin.zfill(n)
+
+        answer_bin=''
+
+        for j in range(n):
+            if arr1_bin[j]=='1' or arr2_bin[j]=='1':
+                answer_bin+='#'
+            else:
+                answer_bin+=' '
+        answer.append(answer_bin)
+  
+    return answer
+
+print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]),["#####", "# # #", "### #", "#  ##", "#####"])
+print(solution(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]),["######", "###  #", "##  ##", " #### ", " #####", "### # "])
