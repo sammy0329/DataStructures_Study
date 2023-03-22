1bd1d5e36b14718e605b3de0964165c5b32d3e99
Woongineering <82315060+ChoiSeoWoong@users.noreply.github.com>
2023-03-22 23:17:33 +0900

    230322 Commit

--- James/230316_study/프로_49993_스킬트리.java
+++ James/230316_study/프로_49993_스킬트리.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for(int i = 0; i < skill_trees.length; i++) {
+            String tempSkill = skill_trees[i];
+            
+            for(int j = 0; j < skill_trees[i].length(); j++) {
+                String tmpValue = skill_trees[i].substring(j, j + 1);
+                
+                if(!skill.contains(tmpValue)) {
+                    tempSkill = tempSkill.replace(tmpValue, "");   
+                }
+            }
+                        
+            if(skill.indexOf(tempSkill) == 0)
+                answer++;
+        }
+        
+        return answer;
+    }
+}
